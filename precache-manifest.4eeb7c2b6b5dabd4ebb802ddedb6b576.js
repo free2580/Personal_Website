b9ed25809e2bfe2765ea1294995ff538bb9a64f5
free2580 <fridakahsas@yahoo.com>
2019-02-01 12:22:23 -0500

    Updates

---
+++ precache-manifest.4eeb7c2b6b5dabd4ebb802ddedb6b576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5784029b50d43ec69aaf",
+    "url": "/Personal_Website/static/css/main.58e40dd6.chunk.css"
+  },
+  {
+    "revision": "5784029b50d43ec69aaf",
+    "url": "/Personal_Website/static/js/main.5784029b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Personal_Website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ad19f7b9bd2ee44634c4",
+    "url": "/Personal_Website/static/js/runtime~main.ad19f7b9.js"
+  },
+  {
+    "revision": "bee45ef1c92c21b91115a2d3e293b48f",
+    "url": "/Personal_Website/static/media/eng.bee45ef1.jpg"
+  },
+  {
+    "revision": "ac9190bbfea14cb768725408f0c1d6b8",
+    "url": "/Personal_Website/index.html"
+  }
+];
